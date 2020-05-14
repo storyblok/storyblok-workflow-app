ccefa496058886bf97e404fe0e68712d2636dc68
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-05-14 13:55:21 -0300

    view last update data and show a badge when has unpublished changes in the Story

---
+++ components/Bagde.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    :class="`bg-${color}-100 text-${color}-700`"
+    class="px-3 h-6 rounded-full text-xs flex items-center"
+  >
+    <span
+      :class="`bg-${color}-400`"
+      class="w-2 h-2 rounded-full mr-1"
+    />
+
+    <span>
+      <slot />
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Badge',
+  props: {
+    color: {
+      type: String,
+      default: 'red'
+    }
+  }
+}
+</script>
--- components/StoryCard.vue
+++ components/StoryCard.vue
@@ -1,15 +1,11 @@
 <template>
-  <div
-    :style="stageColorData"
-    class="bg-white shadow rounded px-3 pt-3 pb-5 border border-t-8"
-  >
+  <div :style="stageColorData" class="bg-white shadow rounded px-3 pt-3 pb-5 border border-t-8">
     <div class="flex justify-between">
       <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">
         {{ story.name }}
-
-        <span class="text-gray-700 font-normal text-xs block">
-          ({{ story.full_slug }})
-        </span>
+        <span
+          class="text-gray-700 font-normal text-xs block"
+        >({{ story.full_slug }})</span>
       </p>
 
       <img
@@ -18,12 +14,46 @@
         class="w-8 h-8 rounded-full"
       >
     </div>
+
+    <div class="flex mt-4 justify-between items-center">
+      <p class="text-sm text-gray-600">
+        {{ story.updated_at | formatDate }}
+      </p>
+
+      <Bagde v-if="story.unpublished_changes">
+        Unpublished Changes
+      </Bagde>
+    </div>
   </div>
 </template>
 
 <script>
+import Bagde from './Bagde'
+
+const abbrev = [
+  'Jan',
+  'Fev',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+]
+
 export default {
   name: 'StoryCard',
+  components: { Bagde },
+  filters: {
+    formatDate (val) {
+      const date = new Date(val)
+      return `${abbrev[date.getMonth()]} ${date.getDate()}`
+    }
+  },
   props: {
     story: {
       type: Object,
