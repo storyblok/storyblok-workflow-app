ded63204e61679b0828e65cf8e7ba36c821987be
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-05-25 17:59:18 -0300

    setup purge to remove unnecessary css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
+  purge: [
+    './components/**/*.vue',
+    './layouts/**/*.vue',
+    './pages/**/*.vue'
+  ],
   theme: {},
   variants: {},
   plugins: []
